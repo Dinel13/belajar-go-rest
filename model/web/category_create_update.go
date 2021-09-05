9131551d6eb890ca1a4fa3de55c8f6307ad2a7c6
dinel <solahuddin.hafid.tomaballo@gmail.com>
2021-09-06 07:32:11 +0800

    add validator

--- model/web/category_create_update.go
+++ model/web/category_create_update.go
@@ -1,6 +1,6 @@
 package web
 
 type CategoryCreateUpdate struct {
-	Id   int    `json:"id"`
-	Name string `json:"name"`
+	Id   int    `validate : "required"`
+	Name string `json :"name", validate : "required, max=200, min=1"`
 }
